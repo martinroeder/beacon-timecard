5cee6bce08e41cad664e564d0cc233438b01c8c2
Martin Roeder <roederm@gmail.com>
2016-05-30 12:58:40 -0500

    Initial Commit

---
+++ TimeCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeCard
+//
+//  Created by Martin Roeder on 5/30/16.
+//  Copyright © 2016 Martin Roeder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
